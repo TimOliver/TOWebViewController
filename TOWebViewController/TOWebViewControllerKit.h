232abf1d455f02adeb7e6b229672195ac87ffabc
Mica Huynh <mica.huynh@carsales.com.au>
2016-02-25 14:02:44 +1100

    add target and shared scheme

---
+++ TOWebViewController/TOWebViewControllerKit.h
@@ -0,0 +1,27 @@
+//
+//  TOWebViewController.h
+//  TOWebViewController
+//
+//  Created by Mica Huynh on 25/02/2016.
+//  Copyright © 2016 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TOWebViewController.
+FOUNDATION_EXPORT double TOWebViewControllerVersionNumber;
+
+//! Project version string for TOWebViewController.
+FOUNDATION_EXPORT const unsigned char TOWebViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TOWebViewController/PublicHeader.h>
+
+
+#import <TOWebViewController/NJKWebViewProgress.h>
+#import <TOWebViewController/NJKWebViewProgressView.h>
+#import <TOWebViewController/TOWebViewController.h>
+#import <TOWebViewController/TOActivitySafari.h>
+#import <TOWebViewController/TOActivityChrome.h>
+#import <TOWebViewController/UIImage+TOWebViewControllerIcons.h>
+#import <TOWebViewController/OnePasswordExtension.h>
+#import <TOWebViewController/TOWebViewController+1Password.h>
